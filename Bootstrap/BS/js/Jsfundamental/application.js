57a8a5ecd775e497723f10c7382fe2254e72cde3
Vaishnavi Gupta05 <vaishnavi.2025b01010636@abes.ac.in>
2026-03-19 11:31:10 +0530

    commit

---
+++ Bootstrap/BS/js/Jsfundamental/application.js
@@ -0,0 +1,27 @@
+var name = "Vaishnavi gupta";
+var age =18
+;
+console.log(name);
+console.log(age);
+if(true){
+    var age = 18;
+}
+console.log(age);
+const PI = 3.14;
+console.log(PI);
+PI= 3.14159;
+console.log(PI);
+console.log(typeof name);
+console.log(typeof age);
+let data = null;
+// value is intentially set to be null.
+console.log(data);
+let result;
+console.log(result);
+console.log(typeof result);
+let isStudent = true;
+let studentName = "Vaishnavi";
+console.log(isStudent);
+console.log(typeof isStudent);
+
+
